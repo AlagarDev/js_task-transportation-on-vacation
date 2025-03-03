c012b62f93e103baf552b4c5e5cd93d3c318ce40
Andrii Mykhei <alagar050@gmail.com>
2025-03-03 13:51:22 +0100

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let totalCoast = 0;
+  const carCost = 40;
+  let offCost = 0;
+
+  if (days >= 3) {
+    offCost = 20;
+  }
+
+  if (days >= 7) {
+    offCost = 50;
+  }
+
+  totalCoast = carCost * days - offCost;
+
+  return totalCoast;
 }
 
 module.exports = calculateRentalCost;
